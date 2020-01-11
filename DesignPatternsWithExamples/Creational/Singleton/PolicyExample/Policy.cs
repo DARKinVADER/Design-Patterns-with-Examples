4ba3aa8ddb669517e511a2c1491b15016a721850
Gábor Czibik <gabor@czibik.hu>
2020-01-11 18:39:01 +0100

    Singleton Patter - Policy example

---
+++ DesignPatternsWithExamples/Creational/Singleton/PolicyExample/Policy.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatternsWithExamples.Creational.Singleton.PolicyExample
+{
+    public class Policy
+    {
+        private static readonly object _lock = new object();
+        private static Policy _instance;
+        public static Policy Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Policy();
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+    }
+}
---
+++ DesignPatternsWithExmaples.Test/SingletonPolicyExampleTest.cs
@@ -0,0 +1,14 @@
+﻿using DesignPatternsWithExamples.Creational.Singleton.PolicyExample;
+using Xunit;
+
+namespace DesignPatternsWithExmaples.Test
+{
+    public class SingletonPolicyExampleTest
+    {
+        [Fact]
+        public void IsPolicyASingleton()
+        {
+            Assert.Same(Policy.Instance, Policy.Instance);
+        }
+    }
+}
